6e75ce1ff76994d9dc779afa56232f78a72a7e1b
Sandeep Sharma <sandy335582@gmail.com>
2020-02-13 17:20:40 +0530

    bug fixes

--- src/Persons/Persons.js
+++ src/Persons/Persons.js
@@ -5,20 +5,24 @@ import AddPerson from '../AddPerson/AddPerson';
 
 class Persons extends Component {
     state = {
-        newPerson: {},
+        newPerson: null,
         total: 0,
         persons: [
             {
                 name: "Sandeep",
                 amount: 10,
+                id:"1a"
+                
             },
             {
                 name: "Neel",
                 amount: 20,
+                id:"2a"
             },
             {
                 name: "Meet",
-                amount: 30
+                amount: 30,
+                id:"3a"
             },
 
 
@@ -27,12 +31,16 @@ class Persons extends Component {
 
 
     addPersonHandler = () => {
-        let newPersons = this.state.persons;
-        newPersons.push(this.state.newPerson)
+        if(this.state.newPerson){
 
-        this.setState({
-            Persons: newPersons
-        })
+            let newPersons = this.state.persons;
+            newPersons.push(this.state.newPerson)
+            this.setState({
+                Persons: newPersons,
+                newPerson:null
+            })
+            
+        }
     }
 
     calculateTotal = () => {
@@ -45,33 +53,38 @@ class Persons extends Component {
 
     nameChangeHandler = (event) => {
         let newName = event.target.value;
+        let newPerson= {
+            name: newName,
+            amount: 0,
+            id:newName.substr(1,3)+Math.random()
+        }
+
         this.setState({
-            newPerson: {
-                name: newName,
-                amount: 0
-            }
+        newPerson: newPerson   
         })
+        
     }
 
-    render() {
-        return (<div className="container">
-            <AddPerson
-                AddPerson={this.addPersonHandler}
-                onNameChange={this.nameChangeHandler} />
 
-            <div className="total">
-                <h2> Total : {this.calculateTotal()} </h2>
-            </div>
-            
-            {this.state.persons.map((person) => {
-                return (<div>
-                    <Person name={person.name}
-                        amount={person.amount}
-                        key={person.name.substr(0, 3) + Math.random() * 100} />
-                </div>)
-            })}
-        </div>);
-    }
+render() {
+    return (<div className="container">
+        <AddPerson
+            AddPerson={this.addPersonHandler}
+            onNameChange={this.nameChangeHandler} />
+
+        <div className="total">
+            <h2> Total : {this.calculateTotal()} </h2>
+        </div>
+
+        {this.state.persons.map((person) => {
+            return (<Person name={person.name}
+                amount={person.amount}
+                key={person.id}
+             />
+            )
+        })}
+    </div>);
+}
 
 }
 export default Persons;
