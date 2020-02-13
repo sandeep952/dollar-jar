59f381eacbe601fde90b19a199b09cb6c4f96cff
Sandeep Sharma <sandy335582@gmail.com>
2020-02-13 14:27:37 +0530

    basic setup with persons component

---
+++ src/Persons/Person/Person.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+let Person = (props)=>{
+    return(<div>
+        I am a person
+        
+        </div>);
+}
+export default Person;
---
+++ src/Persons/Persons.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Person from './Person/Person'
+
+let Persons = (props)=>{
+    return(<div>
+        <Person/>
+        <Person/>
+        
+        </div>);
+}
+export default Persons;
