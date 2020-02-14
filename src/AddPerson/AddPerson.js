7be3e5ef7b9ef3c5e8a74e647c118c5a9589e126
Sandeep Sharma <sandy335582@gmail.com>
2020-02-14 18:46:26 +0530

    refactored AddPerson component

--- src/AddPerson/AddPerson.js
+++ src/AddPerson/AddPerson.js
@@ -27,23 +27,30 @@ class AddPerson extends Component {
 
     }
 
+    handleAddButtonClick() {
+        this.props.addPerson(this.state.name)
+        
+        this.setState({
+            name: ""
+        })
+
+    }
+
     render() {
         return (<div className="container text-center">
             <div>
                 <h5>Add Person</h5>
                 <form>
-                
+
                     <input type="text"
                         ref={this.input_name_Ref}
                         onChange={this.handleNameChange.bind(this)}
                         value={this.state.name}
                         placeholder="Enter your name" />
-                    <button type="submit" onClick={(e) => {
-                        e.preventDefault();
-                        this.props.addPerson(this.state.name)
-                        this.setState({
-                            name: ""
-                        })
+                    <button type="submit" onClick={(event) => {
+                        event.preventDefault();
+                        this.handleAddButtonClick();
+                    
                     }}
 
                         className="btn btn-primary"> Add </button>
